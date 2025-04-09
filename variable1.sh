fd7061460698f23ad49a2b034c0a14eb609b6101
krckykrk <krckykrk@gmail.com>
2025-04-09 05:36:35 +0530

    changing with variables

--- variable1.sh
+++ variable1.sh
@@ -1,6 +1,9 @@
 #!bin/bash
 
-echo "ram: Hi ragu, goodmorning"
-echo "ragu: goodmorning, how r you"
-echo "ram: good, what about you"
-echo "ragu: yaa good"
+Person1=ram
+Person2=ragu
+
+echo "$Person1: Hi $Person2, goodmorning"
+echo "$Person2: goodmorning, how r you"
+echo "$Person1: good, what about you"
+echo "$Person2: yaa good"

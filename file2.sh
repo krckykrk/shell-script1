3af426844cea104233e1fc8c75bbeb431ba25484
krckykrk <krckykrk@gmail.com>
2025-04-09 06:40:28 +0530

    updated script

--- file2.sh
+++ file2.sh
@@ -5,3 +5,10 @@ echo "please enter your username"
 read USERNAME
 
 echo "username entered is: $USERNAME"
+
+
+echo "please enter your password"
+
+read PASSWORD
+
+echo "password entered is:$PASSWORD"

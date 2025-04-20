127425524c298ce2e9ccd14a270ee8a1865b5dc9
krckykrk <krckykrk@gmail.com>
2025-04-20 12:08:58 +0530

    updated script

--- cond2.sh
+++ cond2.sh
@@ -9,5 +9,23 @@ then
 
 fi
 
-yum install mysql -y
+yum install postfix -y
+
+if [ $? -ne 0 ]
+then
+     echo "installation of postfix is error"
+     exit 1
+else
+     echo "installation of postfix is success"
+fi
+
+yum install maven -y
+
+if [ $? -ne 0 ]
+then
+    echo "Installation of maven is error"
+    exit 1
+else
+    echo "installation of maven is success"
+fi        
 

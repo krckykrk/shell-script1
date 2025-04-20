81ead6fe6afac90b67eacffd8c4bf6e06156a179
krckykrk <krckykrk@gmail.com>
2025-04-20 19:57:23 +0530

    updated script

--- functions.sh
+++ functions.sh
@@ -2,6 +2,7 @@
 
 USERID=$(id -u)
 
+#this function should validate the previous command and inform user it is success or failure
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
@@ -11,6 +12,20 @@ VALIDATE(){
         echo "Installation...SUCCESS" 
     fi       
 }
+
+if [ $USERID -ne 0 ]
+then 
+    echo "ERROR::  please run the script with root access"
+    exit 1
+fi
+
+yum install postfix -y
+
+VALIDATE $?
+
+yum install maven -y
+
+VALIDATE $?
 
 
 
